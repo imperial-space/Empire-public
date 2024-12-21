3c27539a18fd52c084c68bab7a8bc6f29a103911
The_Fiafi <thewaffelfiafi@gmail.com>
2024-12-21 03:57:05 +0300

    removing a couple of unused systems

--- Content.Shared/Imperial/Pixelize/PixelizeComponent.cs
+++
@@ -1,8 +0,0 @@
-namespace Content.Shared.Imperial.Pixelize;
-
-
-/// <summary>
-/// Тег-компонент, для использования его на клиенте.
-/// </summary>
-[RegisterComponent]
-public sealed partial class PixelizeComponent : Component;
