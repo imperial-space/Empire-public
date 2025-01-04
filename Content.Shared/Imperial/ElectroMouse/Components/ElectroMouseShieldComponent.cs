a5d4c07e9a5d53b41e3a909b6fdf1b12e480410a
mirton14 <60696783+mirton14@users.noreply.github.com>
2025-01-04 05:46:57 +0000

    Revert "шокомыш"

--- Content.Shared/Imperial/ElectroMouse/Components/ElectroMouseShieldComponent.cs
+++
@@ -1,6 +0,0 @@
-using Robust.Shared.GameStates;
-
-namespace Content.Shared.Imperial.ElectroMouseShield.Components;
-
-[RegisterComponent, NetworkedComponent]
-public sealed partial class ElectroMouseShieldComponent : Component { }
